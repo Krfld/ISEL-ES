177a943cf588e3391130171daa62f7317e7ab460
Krfld <rodrigofldias@gmail.com>
2021-11-12 14:54:24 +0000

    .

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:shopping_list/screens/groups.dart';
 
 import './.imports.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
