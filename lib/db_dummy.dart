cd958bb47edb3eb790032905d890d58af1f9ad36
Krfld <rodrigofldias@gmail.com>
2021-11-17 00:52:06 +0000

    .

--- lib/db_dummy.dart
+++ lib/db_dummy.dart
@@ -1,14 +1,25 @@
-Map dbV2 = {
-  'users': {
-    'userId1': {
-      'name': String,
-      'groups': ['groupId1', 'groupId2'],
-    },
-    'userId2': {
-      'name': String,
-      'groups': ['groupId3', 'groupId4'],
-    },
+Map users = {
+  'userId1': {
+    'name': String,
+    'groups': ['groupId1', 'groupId2'],
   },
+  'userId2': {
+    'name': String,
+    'groups': ['groupId3', 'groupId4'],
+  },
+};
+
+Map product = {
+  'added': int,
+  'name': String,
+  'brand': String,
+  'details': String,
+  'amount': int,
+  'flag': bool,
+};
+
+Map dbV2 = {
+  'users': users,
   'groups': {
     'groupId1': {'name': String},
     'groupId2': {'name': String},
@@ -35,16 +46,8 @@ Map dbV2 = {
   },
 };
 
-Map product = {
-  'added': int,
-  'name': String,
-  'brand': String,
-  'details': String,
-  'amount': int,
-  'flag': bool,
-};
-
 Map dbV1 = {
+  'users': users,
   'groups': {
     'groupId1': {
       'name': String,
@@ -52,23 +55,37 @@ Map dbV1 = {
         'listId1': {
           'name': String,
           'products': {
-            'productId1': {
-              'added': int,
-              'name': String,
-              'brand': String,
-              'details': String,
-              'amount': int,
-              'flag': bool,
-            },
+            'productId1': product,
+            'productId2': product,
+          },
+        },
+        'listId2': {
+          'name': String,
+          'products': {
+            'productId3': product,
+            'productId4': product,
           },
         },
       },
     },
-  },
-  'users': {
-    'userId1': {
+    'groupId2': {
       'name': String,
-      'groups': ['groupId1'],
+      'lists': {
+        'listId3': {
+          'name': String,
+          'products': {
+            'productId5': product,
+            'productId6': product,
+          },
+        },
+        'listId4': {
+          'name': String,
+          'products': {
+            'productId7': product,
+            'productId8': product,
+          },
+        },
+      },
     },
   },
 };
