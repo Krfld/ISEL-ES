02e68bda33a64a68c1077d29d584eca30248234f
Krfld <rodrigofldias@gmail.com>
2022-01-14 14:43:09 +0000

    .

---
+++ flutter_criar_grupo/lib/imports.dart
@@ -0,0 +1,16 @@
+/// Views
+export './views/groups.dart';
+
+/// Services
+export './services/groups_service.dart';
+
+/// Entities
+export './entities/group.dart';
+
+/// Modules
+export './modules/tools.dart';
+export './modules/widgets.dart';
+
+/// Packages
+export 'package:flutter_spinkit/flutter_spinkit.dart';
+export 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
---
+++ flutter_criar_grupo/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart' show Firebase;
+
+import './imports.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping List',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      routes: {
+        'Groups': (context) => Groups(),
+      },
+      home: Groups(),
+    );
+  }
+}
